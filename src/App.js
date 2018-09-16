4631d9763bae80b1e1f8ed1e3b318b986d6f988b
baltazarparra <baltazarparra@outlook.com>
2018-09-16 15:33:14 -0300

    Finish game engine

--- src/App.js
+++ src/App.js
@@ -46,16 +46,58 @@ class App extends Component {
     const choice = (Math.random() * 3 | 0) + 1
     switch (choice) {
       case 1:
-        this.setState({ friendChoice: 'rock' })
+        this.setState({ friendChoice: 'rock' }, () => {
+          this.handleRock(this.state.userChoice)
+        })
         break
       case 2:
-        this.setState({ friendChoice: 'paper' })
+        this.setState({ friendChoice: 'paper' }, () => {
+          this.handlePaper(this.state.userChoice)
+        })
         break
       case 3:
-        this.setState({ friendChoice: 'scissor' })
+        this.setState({ friendChoice: 'scissor' }, () => {
+          this.handleScissor(this.state.userChoice)
+        })
         break
       default:
         break
+    }
+  }
+
+  handleRock = (userEntry) => {
+    let yourPrevScore = this.state.yourScore
+    let yourNewScore = yourPrevScore + 1
+    let friendPrevScore = this.state.friendScore
+    let friendNewScore = friendPrevScore + 1
+    if (userEntry === 'paper') {
+      this.setState({ yourScore: yourNewScore })
+    } else if (userEntry === 'scissor') {
+      this.setState({ friendScore: friendNewScore })
+    }
+  }
+
+  handlePaper = (userEntry) => {
+    let yourPrevScore = this.state.yourScore
+    let yourNewScore = yourPrevScore + 1
+    let friendPrevScore = this.state.friendScore
+    let friendNewScore = friendPrevScore + 1
+    if (userEntry === 'scissor') {
+      this.setState({ yourScore: yourNewScore })
+    } else if (userEntry === 'rock') {
+      this.setState({ friendScore: friendNewScore })
+    }
+  }
+
+  handleScissor = (userEntry) => {
+    let yourPrevScore = this.state.yourScore
+    let yourNewScore = yourPrevScore + 1
+    let friendPrevScore = this.state.friendScore
+    let friendNewScore = friendPrevScore + 1
+    if (userEntry === 'rock') {
+      this.setState({ yourScore: yourNewScore })
+    } else if (userEntry === 'paper') {
+      this.setState({ friendScore: friendNewScore })
     }
   }
 
--- src/components/duel.js
+++ src/components/duel.js
@@ -5,13 +5,13 @@ import scissor from '../scissor.svg'
 
 const Duel = ({ userChoice, friendChoice }) => (
   <section>
-    {userChoice === 'rock' && <img className="your-choice" src={rock} alt="rock" />}
-    {userChoice === 'paper' && <img className="your-choice" src={paper} alt="paper" />}
-    {userChoice === 'scissor' && <img className="your-choice" src={scissor} alt="scissor" />}
-    <h1 className="play-title">Play!</h1>
     {friendChoice === 'rock' && <img className="friend-choice" src={rock} alt="rock" />}
     {friendChoice === 'paper' && <img className="friend-choice" src={paper} alt="paper" />}
     {friendChoice === 'scissor' && <img className="friend-choice" src={scissor} alt="scissor" />}
+    <h1 className="play-title">Play!</h1>
+    {userChoice === 'rock' && <img className="your-choice" src={rock} alt="rock" />}
+    {userChoice === 'paper' && <img className="your-choice" src={paper} alt="paper" />}
+    {userChoice === 'scissor' && <img className="your-choice" src={scissor} alt="scissor" />}
   </section>
 )
 
